1728b08d1aa5323b5c7f75900ca5d5fc6774e8ba
Lucas <Lucas015x@gmail.com>
2025-03-18 22:50:02 -0300

    feat: termo final, para que o valor de uma variável nunca mude

--- anatomia-das-classes/MinhaClasse.java
+++ anatomia-das-classes/MinhaClasse.java
@@ -1,5 +1,7 @@
 public class MinhaClasse {
     public static void main(String[] args) {
         System.out.println("Bem-vindo(a)! Você está explorando as Anatomias das Classes!");
+
+        final String BR = "Brasil"; // final: usado para que o valor de uma variável nunca seja modificada
     }
 }
